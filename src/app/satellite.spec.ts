146bc78eddccbf03290a755e637ac30345350154
Hope <JH075759@cerner.net>
2019-12-29 15:59:58 -0600

    done with section 1 hopefully

--- src/app/satellite.spec.ts
+++ src/app/satellite.spec.ts
@@ -2,7 +2,9 @@ import { Satellite } from './satellite';
 
 describe('Satellite', () => {
   it('should create an instance', () => {
-    expect(new Satellite()).toBeTruthy();
+    expect(new Satellite("SiriusXM", "Communication", "2009-03-21", "LOW", true)).toBeTruthy();
     
   });
 });
+
+
