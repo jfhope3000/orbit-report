509666b8ca1fb793362fcbc8acbc0a0885f47324
Hope <JH075759@cerner.net>
2019-12-30 23:34:36 -0600

    section 6 in progress

--- src/app/satellite.ts
+++ src/app/satellite.ts
@@ -4,12 +4,24 @@ export class Satellite {
     type: string;
     operational: boolean;
     launchDate: string;
+    warning: boolean;
 
-    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean) {
+    constructor(name: string, type: string, launchDate: string, orbitType: string, operational: boolean, warning: boolean) {
         this.name = name;
         this.type = type;
         this.launchDate = launchDate;
         this.orbitType = orbitType;
         this.operational = operational;
+        this.warning = warning;
+       
+    }
+
+    shouldShowWarning() {
+        let type = this.type;
+            if (type === "Space Debris") {
+                this.warning = true;
+            } else {
+                this.warning = false;
+            } 
     }
 }
