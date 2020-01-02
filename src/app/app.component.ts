62775cd6686bd9ca0599580daddb2520b288719f
Hope <JH075759@cerner.net>
2020-01-01 23:46:15 -0600

    finished section 6, working on section 8 ... so close!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,9 @@ export class AppComponent {
    title = 'orbit-report';
    sourceList: Satellite[];
    displayList: Satellite[];
+   
    constructor() {
+      
       this.sourceList = [];
       let satellitesUrl = 'https://handlers.education.launchcode.org/static/satellites.json';
       this.displayList = [];
@@ -20,13 +22,10 @@ export class AppComponent {
       
       window.fetch(satellitesUrl).then(function (response) {
          response.json().then(function (data) {
+            
             let fetchedSatellites = data.satellites;
             for (let i = 0; i < fetchedSatellites.length; i++) {
-               let satellite = new Satellite(fetchedSatellites[i].name,
-                  fetchedSatellites[i].type,
-                  fetchedSatellites[i].launchDate,
-                  fetchedSatellites[i].orbitType,
-                  fetchedSatellites[i].operational);
+               let satellite = new Satellite(fetchedSatellites[i].name, fetchedSatellites[i].type, fetchedSatellites[i].launchDate, fetchedSatellites[i].orbitType, fetchedSatellites[i].operational);
                this.sourceList.push(satellite);
             }
             // make a copy of the sourceList to be shown to the user
@@ -38,7 +37,7 @@ export class AppComponent {
    search(searchTerm: string): void {
       let matchingSatellites: Satellite[] = [];
       searchTerm = searchTerm.toLowerCase();
-      for (let i = 0; i < this.sourceList.length; i++) {
+      for(let i=0; i < this.sourceList.length; i++) {
          let name = this.sourceList[i].name.toLowerCase();
          if (name.indexOf(searchTerm) >= 0) {
             matchingSatellites.push(this.sourceList[i]);
